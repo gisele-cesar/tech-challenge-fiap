fcddee4582b847d0d0177185156ecb2a611ee66a
lmunarim <lmunarim@gmail.com>
2024-10-27 21:03:56 -0300

    Correção script banco de dados

--- scripts/ScriptCriacaoTabelas.sql
+++ scripts/ScriptCriacaoTabelas.sql
@@ -1,70 +1,89 @@
 
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'PosTechFiap')
-    CREATE DATABASE PosTechFiap;
+  CREATE DATABASE PosTechFiap;
+GO
 
 USE PosTechFiap;
+GO
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Cliente')
-BEGIN
-    CREATE TABLE Cliente (
-        IdCliente IDENTITY INT PRIMARY KEY,
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'Pedido')
+	DROP TABLE Pedido;
+GO 
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'StatusPedido')
+	DROP TABLE StatusPedido;
+GO 
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'Produto')
+	DROP TABLE Produto;
+GO 
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'CategoriaProduto')
+	DROP TABLE CategoriaProduto;
+GO 
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'ItemPedido')
+	DROP TABLE ItemPedido;
+GO 
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'Cliente')
+	DROP TABLE Cliente;
+GO 
+
+CREATE TABLE Cliente (
+        IdCliente INT IDENTITY PRIMARY KEY,
         NomeCliente VARCHAR(255) NOT NULL,
         Cpf VARCHAR(11) NOT NULL,
         Email VARCHAR(255) NOT NULL,
-        DataCriacao TIMESTAMP NOT NULL
-    );
-END
+        DataCriacao DATETIME NOT NULL
+);
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'CategoriaProduto')
-BEGIN
-    CREATE TABLE CategoriaProduto (
-        IdCategoriaProduto IDENTITY INT PRIMARY KEY,
+CREATE TABLE CategoriaProduto (
+        IdCategoriaProduto INT IDENTITY PRIMARY KEY,
         NomeCategoriaProduto VARCHAR(255) NOT NULL,
-        DataCriacao TIMESTAMP NOT NULL
+        DataCriacao DATETIME NOT NULL
     );
-END -- subir script de insert das categorias = Lanche, Acompanhamento, Bebida, Sobremesa 
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Produto')
-BEGIN
-    CREATE TABLE Produto (
-        IdProduto IDENTITY INT PRIMARY KEY,
+CREATE TABLE Produto (
+        IdProduto INT IDENTITY PRIMARY KEY,
         IdCategoriaProduto INT,
         NomeProduto VARCHAR(255) NOT NULL,
         DescricaoProduto VARCHAR(255) NOT NULL,
         PrecoProduto DECIMAL NOT NULL,
-        DataCriacao TIMESTAMP NOT NULL,
-        DataAlteracao TIMESTAMP NOT NULL
+        DataCriacao DATETIME NOT NULL,
+        DataAlteracao DATETIME NOT NULL,
         CONSTRAINT FK_CategoriaProduto FOREIGN KEY (IdCategoriaProduto) REFERENCES CategoriaProduto(IdCategoriaProduto)
     );
-END
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'StatusPedido')
-BEGIN
     CREATE TABLE StatusPedido (
-        IdStatus IDENTITY INT PRIMARY KEY,
+        IdStatus INT IDENTITY PRIMARY KEY,
         DescricaoStatus VARCHAR(50) NOT NULL
     )
-END
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Pedido')
-BEGIN
+
     CREATE TABLE Pedido (
-        IdPedido IDENTITY INT PRIMARY KEY,
+        IdPedido INT IDENTITY PRIMARY KEY,
         IdCliente INT NOT NULL,
         NumeroPedido VARCHAR(255) NOT NULL,
         IdStatus INT NOT NULL,
         ValorTotalPedido DECIMAL NOT NULL,
-        DataCriacao TIMESTAMP NOT NULL,
-        DataAlteracao TIMESTAMP NOT NULL
-        CONSTRAINT FK_Cliente FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente)
+        DataCriacao DATETIME NOT NULL,
+        DataAlteracao DATETIME NOT NULL,
+        CONSTRAINT FK_Cliente FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
         CONSTRAINT FK_StatusPedido FOREIGN KEY (IdStatus) REFERENCES StatusPedido(IdStatus)
     );
-END
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'ItemPedido')
-BEGIN
+
     CREATE TABLE ItemPedido (
         IdPedido INT NOT NULL,
         IdProduto INT NOT NULL
     );
-END
+
+go
+
+INSERT INTO CategoriaProduto(NomeCategoriaProduto, DataCriacao)
+VALUES ('Lanche', getdate()), 
+	   ('Acompanhamento', getdate()), 
+	   ('Bebida', getdate()), 
+	   ('Sobremesa', getdate());
+
+INSERT INTO StatusPedido(DescricaoStatus)
+VALUES ('Recebido'), 
+	   ('Em preparação'), 
+	   ('Pronto'), 
+	   ('Finalizado');
--- scripts/ScriptInsertTabelasDominio.sql
+++
@@ -1,7 +0,0 @@
-USE PosTechFiap;
-
-INSERT INTO CategoriaProduto(NomeCategoriaProduto, DataCriacao)
-VALUES (('Lanche', CURRENT_TIMESTAMP), ('Acompanhamento', CURRENT_TIMESTAMP), ('Bebida', CURRENT_TIMESTAMP), ('Sobremesa', CURRENT_TIMESTAMP),)
-
-INSERT INTO StatusPedido(DescricaoStatus)
-VALUES (('Recebido'), ('Em preparação'), ('Pronto'), ('Finalizado'))
