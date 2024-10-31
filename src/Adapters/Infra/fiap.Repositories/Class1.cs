07be5d5ea1c6298e46fa308ce0e7fec061191c20
lmunarim <lmunarim@gmail.com>
2024-10-31 00:36:01 -0300

    .

--- src/Adapters/Infra/fiap.Repositories/Class1.cs
+++ src/Adapters/Infra/fiap.Repositories/Class1.cs
@@ -1,7 +1,21 @@
-﻿namespace fiap.Repositories
-{
-    public class Class1
-    {
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
+namespace fiap.Repositories
+{
+    public class ConexaoBanco
+    {
+        private readonly IConfiguration _config;
+        public ConexaoBanco(IConfiguration config)
+        {
+            _config = config;    
+        }
+        public IDbConnection ObterConexao()
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("fiap.sqlServer"));
+            dbConnection.Open();
+            return dbConnection;
+        }
     }
 }
