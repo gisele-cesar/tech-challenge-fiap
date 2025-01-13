4ab0e32e086f2d84a97b11fec27ca799fa836826
lmunarim <lmunarim@gmail.com>
2025-01-12 21:36:44 -0300

    teste

--- src/Ports/fiap.API/Program.cs
+++ src/Ports/fiap.API/Program.cs
@@ -67,15 +67,17 @@ builder.Services.AddSingleton<Func<IDbConnection>>( sp => {
 builder.Services.AddRepositoriesModule();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(opt =>
     {
         opt.SwaggerEndpoint("/swagger/v1/swagger.json", "FIAP - Tech Challenge V1");
+
+        if (!app.Environment.IsDevelopment())
+        {
+            opt.RoutePrefix = "";
+        }
     });
-}
+
 
 ///app.UseHttpsRedirection();
 app.UseSerilogRequestLogging();
