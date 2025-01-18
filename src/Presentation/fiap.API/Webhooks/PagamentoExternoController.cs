3b997db91879a37e01f1ca9b79f5d1c29afe2dac
Gisele Cesar <gisele-cesar@hotmail.com>
2025-01-18 19:11:21 -0300

    Add summary para apis PagamentoExterno

--- src/Presentation/fiap.API/Webhooks/PagamentoExternoController.cs
+++ src/Presentation/fiap.API/Webhooks/PagamentoExternoController.cs
@@ -16,15 +16,34 @@ namespace fiap.API.Webhooks
             _pedidoApplication = pedidoApplication;
         }
 
-        [HttpPost("ordemCriada")]
-        public async Task<IActionResult> OrdemCriada([FromQuery] string id, [FromQuery] string topic, [FromBody] dynamic content)
+        /// <summary>
+        /// Receber evento de ordem de pagamento criada no meio de pagamento externo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="topic"></param>
+        /// <param name="content"></param>
+        /// <returns>Retorna ok para confirmação de recepção do evento de ordem criada no meio de pagamento externo</returns>
+        /// <response code = "200">Retorna ok para confirmação de recepção do evento</response>
+        /// <response code = "400">Se houver erro na busca do pedido para a ordem criada</response>
+        /// <response code = "500">Se houver erro de conexão com banco de dados</response>
+        [HttpPost("ReceberEventoOrdemCriada")]
+        public async Task<IActionResult> ReceberEventoOrdemCriada([FromQuery] string id, [FromQuery] string topic, [FromBody] dynamic content)
         {
             return Ok(await _pagamentoApplication.ConsultarOrdemPagamento(id));
         }
 
-
-        [HttpPost("pagamentoProcessado")]
-        public async Task<IActionResult> PagamentoProcessado([FromQuery] int data_id, [FromQuery] string topic, [FromBody] dynamic content)
+        /// <summary>
+        /// Receber evento de pagamento processado no meio de pagamento externo
+        /// </summary>
+        /// <param name="data_id"></param>
+        /// <param name="topic"></param>
+        /// <param name="content"></param>
+        /// <returns>Retorna ok para confirmação de recepção do evento de pagamento processado no meio de pagamento externo</returns>
+        /// <response code = "200">Retorna ok para confirmação de recepção do evento</response>
+        /// <response code = "400">Se houver erro na busca por pedidos</response>
+        /// <response code = "500">Se houver erro de conexão com banco de dados</response>
+        [HttpPost("ReceberEventoPagamentoProcessado")]
+        public async Task<IActionResult> ReceberEventoPagamentoProcessado([FromQuery] int data_id, [FromQuery] string topic, [FromBody] dynamic content)
         {
             var pedido = await _pedidoApplication.ObterPedido(data_id);
 
