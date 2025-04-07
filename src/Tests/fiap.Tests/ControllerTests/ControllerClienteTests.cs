54438e009e9aca28a8c617d92f82ec634c33525e
lmunarim <lmunarim@gmail.com>
2025-04-06 21:34:19 -0300

    testes sonar

---
+++ src/Tests/fiap.Tests/ControllerTests/ControllerClienteTests.cs
@@ -0,0 +1,30 @@
+﻿using fiap.API.Controllers;
+using fiap.Application.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiap.Tests.ControllerTests
+{
+    public class ControllerClienteTests
+    {
+        
+        [Fact]
+        public async Task Get_OKAsync()
+        {
+            var _clienteApplication = new Mock<IClienteApplication>();
+            var _logger = new Mock<Serilog.ILogger>();
+
+            _clienteApplication.Setup(x => x.Obter(It.IsAny<int>())).ReturnsAsync(new Domain.Entities.Cliente { Cpf = "12345678910" , Email = "teste@teste.com", Id = 1, Nome = "Joao da Silva" });
+
+            ClienteController clienteController = new(_logger.Object , _clienteApplication.Object);
+
+            var result = await clienteController.Get();
+
+            Assert.NotNull(result);
+        }
+    }
+}
